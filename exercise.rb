4a740eb2c88a343c7c313ebbb796c75ed6112ed5
Graham <grahamspence@gmail.com>
2018-07-24 16:03:42 -0400

    finished exercise

--- exercise.rb
+++ exercise.rb
@@ -1,8 +1,3 @@
-emotions = { happiness: 3, sadness: 1, anger: 1, confusion: 2 }
-
-attr_accessor:name
-attr_accessor:emotions
-
 class Person
 
   def initialize(name, emotions)
@@ -10,14 +5,34 @@ class Person
     @emotions = emotions
   end
 
-  def name(string)
-
-  end
-
-  def emotions(hash)
-
+  def feelings
+    @emotions.each do |feels, num|
+      if num == 1
+        puts "#{@name} is feeling a low amount of #{feels}."
+      elsif num == 2
+        puts "#{@name} is feeling a moderate amount of #{feels}."
+      elsif num == 3
+        puts "#{@name} is feeling a high amount of #{feels}."
+      else
+        puts "Error"
+      end
+    end
   end
 
 end
 
-puts "I am feeling a #{@level} amount of #{@emotions}"
+graham = Person.new("Graham", emotions = {
+  happiness: 3, sadness: 1, hope: 3, anger: 1, confusion: 2
+} )
+
+spiderman = Person.new("Spiderman", emotions = {
+  energy: 3, seriousness: 2, evil: 1
+} )
+
+mario = Person.new("Mario", emotions = {
+  size: 2, verticality: 3, mustachiness: 3, greenness: 1,
+  })
+
+graham.feelings
+spiderman.feelings
+mario.feelings
