7633e6f9aea9e3762f65d605dbd20cb58c226cc6
Graham <grahamspence@gmail.com>
2018-07-24 13:18:48 -0400

    exercise 1 complete

---
+++ exercise1.rb
@@ -0,0 +1,1 @@
+emotions = { "happiness" => 3, "sadness" => 1, "anger" => 1, "confusion" => 2 }
---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+require_relative "exercise1"
+
+class Person
+
+  def initialize(person)
+    @person = person
+  end
+
+  def name(string)
+
+  end
+
+  def emotions(hash)
+
+  end
+
+end
